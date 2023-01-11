111157650fd1554c5116dec9debc57614efffa12
Harmony-Mukolwe <harmonymukolwe@gmail.com>
2023-01-11 12:14:38 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '156eb96e-5408-41ff-8418-b4844be9a29c';
+export const MODAL_CLOSE_SEC = 2.5;
